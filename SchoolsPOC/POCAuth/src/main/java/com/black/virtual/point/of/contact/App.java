9f5b68aae7f5c60f3e3e8a136e52a4d956036b4f
sivanagit <sivanagu.wl@gmail.com>
2017-07-21 10:16:02 +0530

    school

---
+++ SchoolsPOC/POCAuth/src/main/java/com/black/virtual/point/of/contact/App.java
@@ -0,0 +1,13 @@
+package com.black.virtual.point.of.contact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
